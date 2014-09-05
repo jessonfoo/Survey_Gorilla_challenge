b07bf854841061470bf7fcad74e5e2fc68ab4b2d
Andrew McClellan & NokoGary 3 JF <pair+Roomack.ghammell@devbootcamp.com>
2014-09-04 17:55:45 -0700

    finished survey create page

---
+++ public/js/create_survey.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+  var question_count = 0
+  var choice_count = 0
+
+  $("#add_question").on("click", function(event){
+    question_count += 1
+    event.preventDefault()
+    $.ajax({
+      url: '/add_question',
+      type: 'GET',
+      data: {question_id: question_count}
+    })
+    .done(function(data) {
+      $(".question_container").append(data)
+    })
+    .fail( function() {console.log("fail")} )
+  });
+
+  $(".question_container").on("click", ".add_choice", function(event){
+    choice_count += 1
+    button = $(this)
+    event.preventDefault()
+    $.ajax({
+      url: '/add_choice',
+      type: 'GET',
+      data: {choice_id: choice_count}
+    })
+    .done(function(data) {
+      button.parent().append(data)
+    })
+    .fail( function() {console.log("fail")} )
+  });
+
+  $(".question_container").on("click", ".delete_choice", function(event){
+    button = $(this)
+    event.preventDefault()
+    $.ajax({
+    })
+    .done(function(data) {
+      button.parent().remove()
+    })
+    .fail( function() {console.log("fail")} )
+  });
+
+  $("#create_button").on("click", function(event) {
+    event.preventDefault()
+    var form_data = $(".question_container").serialize()
+    console.log(form_data)
+    $.ajax({
+      url: '/users/3/create',
+      type: 'POST',
+      data: form_data
+    })
+    .done(function(data) {
+    })
+    .fail( function() {console.log("fail")} )
+  });
+});
+
+
